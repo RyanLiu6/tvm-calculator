1fb461ba971ad4b1d52606177f57380f3ab25449
Ryan Liu <dio.ryanliu@hotmail.com>
2018-04-15 22:52:18 -0700

    Adding MongoDB

--- api/routes.js
+++ api/routes.js
@@ -2,7 +2,14 @@
 
 const Utils = require("./utils");
 
-module.exports = function(app) {
+module.exports = function(app, db) {
+  const history = db.collection("history");
+  const info = db.collection("info");
+  const blist = db.collection("blist");
+  const wlist = db.collection("wlist");
+  const statusDB = db.collection("status");
+  const images = db.collection("image");
+  const delivery = db.collection("delivery");
 /******************************************************************************/
   app.get("/history", (req, res) => {
     console.log("Hello b0ss");
--- api/server.js
+++ api/server.js
@@ -1,16 +1,26 @@
 // server.js
 
 const express        = require("express");
-const app            = express();
+const MongoClient    = require("mongodb").MongoClient;
 const bodyParser     = require("body-parser");
-const routes         = require("./routes")(app);
+const app            = express();
 
 const port = 3030;
+const localUrl = "mongodb://localhost:27017";
+const dbName = "mainDB";
 
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true}));
 
 app.use(bodyParser.json({ limit: "50mb", extended: true}));
 
-app.listen(port, "0.0.0.0", () => {
-  console.log("We are live on " + port);
+MongoClient.connect(localUrl, (err, database) => {
+  if (err)
+    return console.log(err)
+
+  db = database.db(dbName)
+  require("./app/routes")(app, db)
+
+  app.listen(port, "0.0.0.0", () => {
+    console.log("We are live on " + port);
+  })
 })
