a085bd1c1589a2ce5ab066a7e48ef7d6d4807efd
Ryan Liu <dio.ryanliu@hotmail.com>
2018-04-22 20:50:46 -0700

    Updating web app

---
+++ web/src/components/Test.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Welcome to Test</h2>
+        </div>
+    );
+  }
+}
+
+export default Test;
--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './components/registerServiceWorker';
+import { Element, Events, scrollSpy, animateScroll as scroll } from 'react-scroll';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import App from './components/App';
+import Test from './components/Test';
+import Intro from './components/Intro';
+import './index.css';
+
+// class IndexPage extends Component {
+//   componentDidMount() {
+//     Events.scrollEvent.register('begin', (to, element) => {
+//       console.log('begin', arguments)
+//     })
+//
+//     Events.scrollEvent.register('end', function(to, element) {
+//       console.log('end', arguments)
+//     })
+//
+//     scrollSpy.update()
+//   }
+//
+//   componentWillMount() {
+//     Events.scrollEvent.remove('begin')
+//     Events.scrollEvent.remove('end')
+//   }
+//
+//   render() {
+//     console.log("FUcK");
+//     return (
+//       <div style={{ height: '100%' }}>
+//         <Intro />
+//       </div>
+//     )
+//   }
+// }
+
+ReactDOM.render(
+  <div>
+    <Intro />
+    <App />
+  </div>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
